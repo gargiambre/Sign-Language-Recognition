f84541626d050ef675e0e4dd7a8a74a7ca76e620
GargiAmbre <157484050+gargiambre@users.noreply.github.com>
2024-07-02 12:37:18 +0530

    Update test.py

---
+++ test.py
@@ -0,0 +1,71 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
+import tensorflow
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+
+offset = 20
+imgSize = 300
+counter = 0
+
+folder = "Data/"
+
+labels = ["J"]
+
+while True:
+    success, image = cap.read()
+    imgOutput = image.copy()
+    hands, image = detector.findHands(image)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand["bbox"]
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+
+        imgCrop = image[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h/w
+
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize-wCal)/2)                       
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize-hCal)/2)                       
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset-50+50), (255, 0, 255), cv2.FILLED)
+
+        cv2.putText(imgOutput, labels[index], (x, y-27), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255,255, 255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 255), 4)
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+
+    cv2.imshow("Image", imgOutput)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f"{folder}/Image_{time.time()}.jpg", imgWhite)
+        print(counter)
+    elif key == ord("q"):
+        break
